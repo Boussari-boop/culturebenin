26eead82e1780a471c29a17f90a640d7318bcb5e
Dr BOUSSARI <boussarimistourath675@gmail.com>
2025-12-10 12:41:50 +0100

    New provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forcer HTTPS si l'environnement est production
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
